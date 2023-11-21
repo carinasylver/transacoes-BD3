e6dfbc977aad2da7708877b8c9b3420d9cb48f2c
carinasylver <cah.sylver@gmail.com>
2023-11-21 18:48:52 -0300

    Solucao para bloquear o fenomeno de leitura não repetivel

---
+++ 4.1-solucao-para-leituraNaoRepetivel.sql
@@ -0,0 +1,15 @@
+-- transação T1
+BEGIN;
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+	SELECT numero, data_vencimento
+	FROM contas_pagar
+	WHERE TO_CHAR(data_vencimento, 'YYYY-MM') = '2023-12';
+
+-- transação 2
+BEGIN;
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+	UPDATE contas_pagar
+	SET data_vencimento = data_vencimento + INTERVAL '5 days'
+	WHERE TO_CHAR(data_vencimento, 'YYYY-MM') = '2023-12';
+COMMIT;
+
