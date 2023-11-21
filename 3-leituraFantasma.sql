cab4abc0a6141e292af1df9d5b98f82dd5a13453
carinasylver <cah.sylver@gmail.com>
2023-11-21 18:34:33 -0300

    Situação de leitura fantasma

---
+++ 3-leituraFantasma.sql
@@ -0,0 +1,25 @@
+-- transação T1
+BEGIN;
+	SELECT f.nome, v.* 
+  FROM vendas v
+	JOIN funcionarios f USING(codfunc)
+	WHERE f.nome ILIKE 'mariana costa';
+
+-- transação T2
+BEGIN;
+	INSERT INTO ordens_servico (numero, data, valor_total, status, codfunc, codCliente)
+	  VALUES (09, '2023/11/20', 3000, 'f', 201, 105);
+	--DELETE FROM ordens_servico WHERE numero = 09;
+	
+	SELECT * FROM ordens_servico;	
+		
+	INSERT INTO vendas (codigo, valor_total_venda, dt_venda, codfunc, codcliente, numero)
+		VALUES(09, 3000, '2023-11-20', 201, 105, 09);
+	--DELETE FROM vendas WHERE numero = 09;
+COMMIT;
+
+-- transação T1
+  SELECT f.nome, v.* 
+  FROM vendas v
+  JOIN funcionarios f USING(codfunc)
+  WHERE f.nome ILIKE 'mariana costa';
