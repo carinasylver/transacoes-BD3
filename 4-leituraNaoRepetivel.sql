3a449c2efee04a981b6fc098b84a26062179f8a0
carinasylver <cah.sylver@gmail.com>
2023-11-21 19:03:46 -0300

    Situacao de leitura não repetivel

--- 4-leituraNaoRepetivel.sql
+++ 4-leituraNaoRepetivel.sql
@@ -4,7 +4,7 @@ BEGIN;
 	FROM contas_pagar
 	WHERE TO_CHAR(data_vencimento, 'YYYY-MM') = '2023-12';
 
--- transacao T2
+-- transacao 2
 BEGIN;
 	UPDATE contas_pagar
 	SET data_vencimento = data_vencimento + INTERVAL '5 days'
