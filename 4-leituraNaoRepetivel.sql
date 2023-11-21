a9c2f5901651dbf29dddc0300f4c313bab0bbf8f
carinasylver <cah.sylver@gmail.com>
2023-11-21 18:45:09 -0300

    Solucao para bloquear o fenomeno de leitura não repetivel

--- 4-leituraNaoRepetivel.sql
+++ 4-leituraNaoRepetivel.sql
@@ -10,3 +10,8 @@ BEGIN;
 	SET data_vencimento = data_vencimento + INTERVAL '5 days'
 	WHERE TO_CHAR(data_vencimento, 'YYYY-MM') = '2023-12';
 COMMIT;
+
+-- transacao T1
+	SELECT numero, data_vencimento
+	FROM contas_pagar
+	WHERE TO_CHAR(data_vencimento, 'YYYY-MM') = '2023-12';
